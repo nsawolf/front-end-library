a1a5497d03fee77702f0d6441688cf2bf290c391
Adam Schnaare <adam.schnaare@gmail.com>
2013-05-30 12:24:16 -0500

    Book adjustments and Homepage / MainMenu responsive adjustments

--- js/jos/jos-appcanvas.js
+++ js/jos/jos-appcanvas.js
@@ -44,10 +44,12 @@
     };
     Appcanvas.prototype.shift = function(){
         var base = this;
+        console.log('HEYEYEYEEy');
 
-        $(this.$element).on('click', '[data-trigger*="shift-"]', function(){
+        $('body').on('click', '[data-trigger*="shift-"]', function(){
             var trigger = $(this).attr('data-trigger');
             var classList = $(base.$element).attr('class').split(/\s+/);
+
 
             // Remove all "shift" classes
             $.each( classList, function(index, item){
@@ -61,7 +63,7 @@
         });
     };
 
-    // Appcanvas plugin definition
+    // appcanvas plugin definition
     $.fn.appcanvas = function(options){
         return this.each(function(){
             if ( !$.data(this, 'plugin_appcanvas') ){
@@ -78,185 +80,3 @@
     });
 
 }(window.jQuery);
-
-//!function ($)
-//{
-//    "use strict";
-//
-//    $.fn.appcanvas = function( options ) {
-//
-//        var settings = $.extend({
-//            color: "#556b2f",
-//            backgroundColor: "white"
-//        }, options );
-//
-//
-//        return this.each(function(){
-//            var $this = $(this);
-//
-//            // Count Siblings of appcanvas
-//            var siblingsAbove = $this.siblings('.jos-appcanvas-above');
-//            var siblingsBelow = $this.siblings('.jos-appcanvas-below');
-//            var topOffset = 0;
-//            var bottomOffset = 0;
-//
-//            // Calculate the sum of sizes of other siblings
-//            siblingsAbove.each(function(){
-//                topOffset += $(this).outerHeight();
-//            });
-//            siblingsBelow.each(function(){
-//                bottomOffset += $(this).outerHeight();
-//            });
-//
-//            // Set top offset of the appcanvas object
-//            $this.css('top',topOffset);
-//            $this.css('bottom',bottomOffset);
-//
-//            /* Shifting
-//             =================================== */
-//            $('[data-trigger*="shift-"]').on('click',function(){
-//                var trigger = $(this).attr('data-trigger');
-//                var classList = $this.attr('class').split(/\s+/);
-//
-//                $.each( classList, function(index, item){
-//                    // toggle self
-//                    if ( item.match('^shift') ){
-//                        $this.removeClass(item);
-//                    }
-//                });
-//                $this.addClass(trigger);
-//
-//            });
-//        });
-//
-//    };
-//
-//    $('.jos-appcanvas').appcanvas();
-//
-//}(window.jQuery);
-//
-/////* ==================================
-//// AppCanvas - v2
-//// =================================== */
-////
-////$(function(){
-////
-////    /* Module Definition
-////     =================================== */
-////    var josAppCanvas = (function() {
-////
-////        var init = function( settings ) {
-////            // defaults
-////            this.defaults = {
-////                elem: '.jos-appcanvas',
-////                elemAbove: '.jos-appcanvas-above',
-////                elemBelow: '.jos-appcanvas-below',
-////                trigger: 'shift-'
-////            };
-////
-////            // allow overriding the default config
-////            $.extend( this.defaults, settings );
-////
-////            setup();
-////        };
-////
-////        // private variables and functions
-////        var setup = function(){
-////            calcSize();
-////            shift();
-////        };
-////        var calcSize = function(){
-////            var $elem = $(josAppCanvas.defaults.elem);
-////            var $siblingsAbove = $elem.siblings(josAppCanvas.defaults.elemAbove);
-////            var $siblingsBelow = $elem.siblings(josAppCanvas.defaults.elemBelow);
-////            var topOffset = 0;
-////            var bottomOffset =  0;
-////
-////            // Calculate the sum of sizes of other siblings
-////            $siblingsAbove.each(function(){
-////                topOffset += $(this).outerHeight();
-////            });
-////            $siblingsBelow.each(function(){
-////                bottomOffset += $(this).outerHeight();
-////            });
-////
-////            // Set top offset of the appcanvas object
-////            $elem.css('top',topOffset);
-////            $elem.css('bottom',bottomOffset);
-////        };
-////        var shift = function(){
-////            var $elem = $(josAppCanvas.defaults.elem);
-////            $('[data-trigger*="' + josAppCanvas.defaults.trigger + '"]').on('click',function(){
-////                var trigger = $(this).attr('data-trigger');
-////                var classList = $elem.attr('class').split(/\s+/);
-////
-////                $.each( classList, function(index, item){
-////                    // toggle self
-////                    if ( item.match('^shift') ){
-////                        $elem.removeClass(item);
-////                    }
-////                });
-////                $elem.addClass(trigger);
-////            });
-////        };
-////
-////
-////        // public API
-////        return {
-////            init: init
-////        };
-////
-////    })();
-////
-////    /* Usage / Instantiation
-////     =================================== */
-////    josAppCanvas.init();
-////
-////});
-
-
-
-///* ==================================
-// AppCanvas - v3
-// =================================== */
-//
-//$(function(){
-//    "use strict";
-//    var appcanvas = $('.jos-appcanvas');
-//
-//    // Count Siblings of appcanvas
-//    var siblingsAbove = appcanvas.siblings('.jos-appcanvas-above');
-//    var siblingsBelow = appcanvas.siblings('.jos-appcanvas-below');
-//    var topOffset = 0;
-//    var bottomOffset = 0;
-//
-//    // Calculate the sum of sizes of other siblings
-//    siblingsAbove.each(function(){
-//        topOffset += $(this).outerHeight();
-//    });
-//    siblingsBelow.each(function(){
-//        bottomOffset += $(this).outerHeight();
-//    });
-//
-//
-//    // Set top offset of the appcanvas object
-//    appcanvas.css('top',topOffset);
-//    appcanvas.css('bottom',bottomOffset);
-//
-//    /* Shifting
-//     =================================== */
-//    $('[data-trigger*="shift-"]').on('click',function(){
-//        var trigger = $(this).attr('data-trigger');
-//        var classList = appcanvas.attr('class').split(/\s+/);
-//
-//        $.each( classList, function(index, item){
-//            // toggle self
-//            if ( item.match('^shift') ){
-//                appcanvas.removeClass(item);
-//            }
-//        });
-//        appcanvas.addClass(trigger);
-//
-//    });
-//
-//});
