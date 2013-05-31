5610731c52e7ff59d27de2402aea668239bbd704
Adam Schnaare <adam.schnaare@gmail.com>
2013-05-31 17:18:32 -0500

    Book view updated

--- js/jos/jos-book.js
+++ js/jos/jos-book.js
@@ -7,12 +7,38 @@ $(function(){
     =================================== */
     var viewTriggers = $('[data-toggle="view"]');
 
-    // Display view that is referenced by trigger
-    function toggleView(viewTrigger){
+    function switchView(viewTrigger){
         var target = viewTrigger.attr('id');
-        $('.jos-view').hide();
-        $('.' + target).show();
-    }
+
+        // Remove all "shift" classes
+        var classList = $('.jos-page-group').attr('class').split(/\s+/);
+        $.each( classList, function(index, item){
+            if ( item.match('^jos-view-') ){
+                $('.jos-page-group').removeClass(item);
+            }
+        });
+
+        // Add new class
+        $('.jos-page-group').addClass(target);
+
+        // Integrate with pageGroupHeight plugin
+        var maxSize = viewTrigger.attr('data-maxsize');
+        var rows = viewTrigger.attr('data-rows');
+        console.log(target);
+        $('.' + target).pageGroupHeight({
+            rows: rows
+        });
+
+
+        // Integrate with Autocolumn
+        if (maxSize){
+            $('.jos-autocolumn').attr('data-maxsize', maxSize);
+        }
+        else{
+            $('.jos-autocolumn').attr('data-maxsize', '');
+        }
+        autocolumn.size();
+    };
 
     // Display default active trigger
     var activeTrigger = '';
@@ -21,11 +47,12 @@ $(function(){
             activeTrigger = $(this);
         }
     });
-    toggleView(activeTrigger);
+
+    switchView(activeTrigger);
 
     // Activate view on trigger event
     viewTriggers.on('click',function(){
-        toggleView($(this));
+        switchView($(this));
     });
 
     // layer groups (aka tabs)
@@ -64,4 +91,4 @@ $(function(){
 
     // TODO: Remove hoveritem on pretty much any other event
 
-});
+})
--- js/jos/jos-page-group.js
+++ js/jos/jos-page-group.js
@@ -2,27 +2,45 @@
     jos-page-group
 =================================== */
 
-$(function(){
+!function ($)
+{
 
-    // jos-page-group-16
+    "use strict";
 
+    var defaults = {
+        item: '.jos-page-group',
+        rows: 9
+    };
 
+    // PageGroupHeight function definition
+    function PageGroupHeight( $element, options ){
+        var base = this;
+        this.$element = $element;
+        this.options = $.extend( {}, defaults, options );
+        this.init();
 
-    function setHeight(){
-        var appCanvasContent = $('.jos-appcanvas-content');
-        var contentHeight = appCanvasContent.height();
-        var pageGroupItems = $('.jos-page-group-item');
-        var rows = 9;
-        pageGroupItems.outerHeight( Math.floor( contentHeight/rows) );
+        $(window).resize(function(){
+            base.init();
+        });
     }
 
-    setHeight();
-    $(window).resize(function(){
-        setHeight();
-    });
+    // PageGroupHeight prototypes
+    PageGroupHeight.prototype.init = function(){
+        var base = this;
+        this.appCanvasContent = $('.jos-appcanvas-content');
+        this.contentHeight = this.appCanvasContent.height();
+        this.pageGroupItems = $(this.options.item + ' .jos-page-group-item');
+        this.pageGroupItems.outerHeight( Math.floor( this.contentHeight/this.options.rows) );
 
+    };
 
+    // pageGroupHeight plugin definition
+    $.fn.pageGroupHeight = function(options){
+        return this.each(function(){
 
+                $.data(this, 'plugin_pageGroupHeight', new PageGroupHeight( this, options ));
 
+        });
+    };
 
-});
+}(window.jQuery);
